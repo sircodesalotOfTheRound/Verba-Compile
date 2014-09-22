8852604da0c27d5a27622203c989199d4a6b32f2
Reuben Kuhnert <sircodesalot@gmail.com>
2014-09-22 16:01:19 -0500

    Added RetOpCode.java

--- src/main/java/com/verba/language/codegen/opcodes/RetOpcode.java
+++ src/main/java/com/verba/language/codegen/opcodes/RetOpcode.java
@@ -1,7 +1,17 @@
 package com.verba.language.codegen.opcodes;
 
+import com.verba.language.codegen.rendering.OpCodeRenderer;
+
 /**
  * Created by sircodesalot on 14/9/19.
  */
-public class RetOpcode {
+public class RetOpCode implements VerbajOpCode {
+  @Override
+  public int opNumber() { return 0xc7; }
+
+  @Override
+  public String opName() { return "Ret"; }
+
+  @Override
+  public void render(OpCodeRenderer renderer) { /* The op will automatically be written*/ }
 }
